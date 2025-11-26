4473f0aa18dc308b4c4202e3f109bc5530b969fe
andresalidia <andresalidia1@gmail.com>
2025-11-26 10:00:04 -0400

    atualizacao

--- restaurante/src/test/java/br/com/dito/restaurante/RestauranteApplicationTests.java
+++ restaurante/src/test/java/br/com/dito/restaurante/RestauranteApplicationTests.java
@@ -1,4 +1,4 @@
-package restaurante.src.test.java.br.com.dito.restaurante;
+package br.com.dito.restaurante;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
