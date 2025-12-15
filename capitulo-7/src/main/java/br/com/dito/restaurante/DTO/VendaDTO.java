43cba1cc6ccd4dff4eab1685a52217f033058f4a
andresalidia <andresalidia1@gmail.com>
2025-12-15 14:49:09 -0400

    escrita do cap 7

---
+++ capitulo-7/src/main/java/br/com/dito/restaurante/DTO/VendaDTO.java
@@ -0,0 +1,23 @@
+package br.com.dito.restaurante.DTO;
+
+public class VendaProdutoDTO {
+
+    private Long produtoId;
+    private Integer quantidade;
+
+    public Long getProdutoId() {
+        return produtoId;
+    }
+
+    public void setProdutoId(Long produtoId) {
+        this.produtoId = produtoId;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+}
